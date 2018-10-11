99f71ffb2e27b9af93ad94a7ee1c603d0006d990
Carol Ni <carol.ni@sapphireinfo.com.tw>
2018-10-11 17:59:50 +0800

    update readme.md

---
+++ README.md
@@ -0,0 +1,34 @@
+# time-zone-converter
+
+> Easiest way to convert your date time to specific time zone.
+
+## How to Use?
+### Installation
+
+```javascript
+
+# npm 
+npm install timeZoneConverter
+
+# yarn
+yarn add timeZoneConverter
+
+```
+
+### Import
+```javascript
+
+import timeZoneConverter from 'timeZoneConverter'
+
+```
+#### Arguments
+
+```javascript
+
+timeZoneConverter(originalDateTime, originalTimeZone, targetTimeZone, outputFormate)
+
+
+timeZoneConverter('2018/10/11 18:00:00', 8, -4, 'YYYY/MM/DD HH:mm:ss')
+
+
+```
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import moment from 'moment'
 
-const tzConverter = (dateTime, originalTimeZone, targetTimeZone, outputFormat = 'YYYY-MM-DD HH:mm:ss') => {
+const timeZoneConverter = (dateTime, originalTimeZone, targetTimeZone, outputFormat = 'YYYY-MM-DD HH:mm:ss') => {
     const dateTimeStr = dateTime.concat(originalTimeZone >= 0 ? '+' : '-')
         .concat(('0' + Math.abs(originalTimeZone)).slice(-2))
     return moment(dateTimeStr, outputFormat).utcOffset(targetTimeZone).format(outputFormat)
 }
 
-export default tzConverter
+export default timeZoneConverter
