4d68b859374237d7f3250c761a505ece8cd3586e
Carol Ni <carol.ni@sapphireinfo.com.tw>
2018-10-11 14:50:55 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import moment from 'moment'
+
+const tzConverter = (dateTime, originalTimeZone, targetTimeZone, outputFormat = 'YYYY-MM-DD HH:mm:ss') => {
+    const dateTimeStr = dateTime.concat(originalTimeZone >= 0 ? '+' : '-')
+        .concat(('0' + Math.abs(originalTimeZone)).slice(-2))
+    return moment(dateTimeStr).utcOffset(targetTimeZone).format(outputFormat)
+}
+
